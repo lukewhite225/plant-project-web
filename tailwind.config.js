321a2b1569bff88a41d33577b5545db43e90fbe5
lukewhite225 <lukewhi225@gmail.com>
2024-07-14 14:28:03 -0500

    Tailwind and updated

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './plant-web-project/public/index.html',
+    './plant-web-project/public/users.html',
+    './plant-web-project/public/plantmonitor.html'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
